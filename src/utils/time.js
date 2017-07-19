1f568d261ba03a08c93623d6e03a8e5654f7e832
fedor <fedor.korotkov@gmail.com>
2017-07-18 18:45:45 -0700

    added radix

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,6 +1,6 @@
 export function formatDuration(durationInMs) {
   let formatSinglePart = (value) => value < 10 ? "0" + value : value.toString();
-  durationInMs = Math.max(0, parseInt(durationInMs));
+  durationInMs = Math.max(0, parseInt(durationInMs, 10));
   let secondsTotal = durationInMs / 1000;
   let seconds = Math.floor(secondsTotal % 60);
   let minutes = Math.floor(secondsTotal / 60);

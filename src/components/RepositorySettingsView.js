595f28c3474856f231d10494ef06a8cafb7a58f5
fedor <fedor.korotkov@gmail.com>
2017-07-19 09:30:09 -0700

    template for encrypted variables

--- src/components/RepositorySettingsView.js
+++ src/components/RepositorySettingsView.js
@@ -3,7 +3,11 @@ import React from 'react';
 import {createFragmentContainer, graphql,} from 'react-relay';
 import {withRouter} from 'react-router-dom'
 
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import FontIcon from 'material-ui/FontIcon';
 import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar';
 
 
@@ -17,16 +21,43 @@ class RepositorySettingsView extends React.Component {
       main: {
         paddingTop: 8
       },
+      settingGap: {
+        paddingTop: 16
+      },
+      securedVariableTitle: {
+        verticalAlign: "middle"
+      },
     };
 
     return (
       <div style={styles.main} className="container">
-        <Paper zDepth={1} rounded={false}>
+        <Paper style={styles.settingItem} zDepth={1} rounded={false}>
           <Toolbar>
             <ToolbarGroup>
-              <ToolbarTitle text={this.props.repository.fullName + " settings"}/>
+              <ToolbarTitle text={this.props.repository.fullName + " repository settings"}/>
             </ToolbarGroup>
           </Toolbar>
+        </Paper>
+        <div style={styles.settingGap}/>
+        <Paper zDepth={1} rounded={false}>
+          <Card>
+            <CardHeader
+              title="Secured Variables"
+              titleStyle={styles.securedVariableTitle}
+              actAsExpander={true}
+              showExpandableButton={true}
+              avatar={<FontIcon className="material-icons">lock</FontIcon>}
+            />
+            <CardText expandable={true}>
+              <TextField
+                hintText="Enter value to create a secure variable for"
+                multiLine={true}
+                fullWidth={true}/>
+            </CardText>
+            <CardActions expandable={true}>
+              <RaisedButton primary={true} label="Encrypt"/>
+            </CardActions>
+          </Card>
         </Paper>
       </div>
     );

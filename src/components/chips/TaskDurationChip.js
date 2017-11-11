408ce124fc8eeef089580ef61b2e5d95d2f5f21f
fedor <fedor.korotkov@gmail.com>
2017-11-10 18:36:03 -0800

    subscription in `TaskDurationChip`

--- src/components/chips/TaskDurationChip.js
+++ src/components/chips/TaskDurationChip.js
@@ -6,8 +6,47 @@ import FontIcon from 'material-ui/FontIcon';
 import {taskStatusColor} from "../../utils/colors";
 import {formatDuration} from "../../utils/time";
 import {isTaskFinalStatus, taskStatusIconName} from "../../utils/status";
+import {graphql, requestSubscription} from "react-relay";
+import environment from "../../createRelayEnvironment";
+
+const taskSubscription = graphql`
+  subscription TaskDurationChipSubscription(
+    $taskID: ID!
+  ) {
+    task(id: $taskID) {
+      id
+      status
+      creationTimestamp
+      durationInSeconds
+    }
+  }
+`;
 
 class TaskDurationChip extends React.Component {
+  componentDidMount() {
+    if (isTaskFinalStatus(this.props.task.status)) {
+      return
+    }
+
+    let variables = {taskID: this.props.task.id};
+
+    this.subscription = requestSubscription(
+      environment,
+      {
+        subscription: taskSubscription,
+        variables: variables
+      }
+    );
+  }
+
+  componentWillUnmount() {
+    this.closeSubscription();
+  }
+
+  closeSubscription() {
+    this.subscription && this.subscription.dispose && this.subscription.dispose()
+  }
+
   render() {
     let task = this.props.task;
     let durationInSeconds = task.durationInSeconds;

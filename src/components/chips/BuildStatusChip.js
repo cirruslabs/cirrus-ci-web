f076b0821d38d66c6bb8a3a3687291cdee6220ad
fedor <fedor.korotkov@gmail.com>
2017-11-10 18:56:42 -0800

    subscription in `BuildStatusChip`

--- src/components/chips/BuildStatusChip.js
+++ src/components/chips/BuildStatusChip.js
@@ -4,15 +4,53 @@ import Avatar from 'material-ui/Avatar';
 import Chip from 'material-ui/Chip';
 import FontIcon from 'material-ui/FontIcon';
 import {buildStatusColor} from "../../utils/colors";
-import {buildStatusIconName, buildStatusMessage} from "../../utils/status";
+import {buildStatusIconName, buildStatusMessage, isTaskFinalStatus} from "../../utils/status";
+import {graphql, requestSubscription} from "react-relay";
+import environment from "../../createRelayEnvironment";
 
-export default function (props) {
-  let build = props.build;
-  return (
-    <Chip style={props.style}>
-      <Avatar backgroundColor={buildStatusColor(build.status)}
-              icon={<FontIcon className="material-icons">{buildStatusIconName(build.status)}</FontIcon>}/>
-      {buildStatusMessage(build)}
-    </Chip>
-  );
+const buildSubscription = graphql`
+  subscription BuildStatusChipSubscription(
+    $buildID: ID!
+  ) {
+    build(id: $buildID) {      
+      id
+      durationInSeconds
+      status
+    }
+  }
+`;
+
+class BuildStatusChip extends React.Component {
+  componentDidMount() {
+    let variables = {buildID: this.props.build.id};
+
+    this.subscription = requestSubscription(
+      environment,
+      {
+        subscription: buildSubscription,
+        variables: variables
+      }
+    );
+  }
+
+  componentWillUnmount() {
+    this.closeSubscription();
+  }
+
+  closeSubscription() {
+    this.subscription && this.subscription.dispose && this.subscription.dispose()
+  }
+
+  render() {
+    let build = this.props.build;
+    return (
+      <Chip style={this.props.style}>
+        <Avatar backgroundColor={buildStatusColor(build.status)}
+                icon={<FontIcon className="material-icons">{buildStatusIconName(build.status)}</FontIcon>}/>
+        {buildStatusMessage(build)}
+      </Chip>
+    );
+  }
 }
+
+export default BuildStatusChip

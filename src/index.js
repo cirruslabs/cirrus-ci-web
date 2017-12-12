525f67ecc5b1530f94b14a5d3d9eb286d7d3067d
fedor <fedor.korotkov@gmail.com>
2017-12-11 16:30:57 -0800

    disable service worker

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 import './index.css';
 import './rtu/ConnectionManager'
 
 injectTapEventPlugin(); // for fancy material ui effects
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+unregister();

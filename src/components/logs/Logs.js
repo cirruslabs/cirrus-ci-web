a35bb48b247929b9a92f975101f457a23f9cba03
fedor <fedor.korotkov@gmail.com>
2018-02-07 13:20:03 -0500

    xterm logging

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,50 +1,61 @@
 import React from 'react';
+import {Terminal} from 'xterm';
+import 'xterm/dist/xterm.css';
+import * as fit from 'xterm/lib/addons/fit/fit';
 
 class Logs extends React.Component {
   constructor(props) {
     super(props);
-    this.subscriptionClosable = null;
-    this.state = {
-      logLines: props.logLines || [],
-      logTail: ""
-    };
+    Terminal.applyAddon(fit);
+    this.term = new Terminal({
+      cursorBlink: false,
+      cols: 200,
+      disableStdin: true,
+      fontFamily: "Monaco,monospace",
+      fontSize: 12,
+    });
+    this.term.on('open', () => {
+      this.term.fit();
+    })
   }
 
   appendLogs(newLogs) {
-    let currentLogsToAppend = this.state.logTail + newLogs;
-    let newLogLines = currentLogsToAppend.split("\n");
-    let newLogTail = newLogLines.pop();
-    this.setState({
-      logLines: this.state.logLines.concat(newLogLines),
-      logTail: newLogTail
-    })
+    // for some weird reasons just this.term.write(newLogs) doesn't work as intendent
+    let logLines = newLogs.split("\n");
+    if (logLines.length === 0) {
+      return;
+    }
+    this.term.write(logLines.shift());
+    if (logLines.length === 0) {
+      return;
+    }
+    let tail = logLines.pop();
+    logLines.forEach((line) => this.term.writeln(line))
+    this.term.write(tail)
+  }
+
+  componentDidMount() {
+    this.term.open(this.refs.container);
+    if (this.props.logLines) {
+      this.props.logLines.forEach((line) => this.term.writeln(line));
+    }
+  }
+
+  componentWillUnmount() {
+    this.term.destroy()
   }
 
   render() {
     let styles = {
       logContainer: {
         overflowY: "scroll",
-        backgroundColor: "#212121",
         minHeight: "50px",
         maxHeight: "500px",
       },
-      logLine: {
-        color: "#FAFAFA",
-        fontFamily: "Monaco,monospace",
-        fontSize: "12px",
-        paddingLeft: "7px",
-        margin: 0
-      },
     };
 
-    let logLines = this.state.logLines;
     return (
-      <div style={styles.logContainer}>
-        {
-          logLines.map((line, index) => <p key={index} style={styles.logLine}>{line}</p>)
-        }
-        <p>{this.state.logTail}</p>
-      </div>
+      <div style={styles.logContainer} ref="container"/>
     );
   }
 }

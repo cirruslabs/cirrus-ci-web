1ea4526cda887051c8d9a411d70d212f8e0d6459
Fedor Korotkov <fedor.korotkov@gmail.com>
2018-10-14 20:55:20 -0400

    Extracted separate GitHubPurchase components

--- src/components/GitHubOrganizationSettings.js
+++ src/components/GitHubOrganizationSettings.js
@@ -4,22 +4,11 @@ import {withRouter} from 'react-router-dom'
 import {createFragmentContainer, graphql} from 'react-relay';
 import Paper from '@material-ui/core/Paper';
 import {withStyles} from "@material-ui/core";
-import Button from "@material-ui/core/Button/Button";
-import Card from "@material-ui/core/Card/Card";
-import CardContent from "@material-ui/core/CardContent/CardContent";
 import Typography from "@material-ui/core/Typography/Typography";
-import CardActions from "@material-ui/core/CardActions/CardActions";
-import CardHeader from "@material-ui/core/CardHeader/CardHeader";
-import Icon from "@material-ui/core/Icon/Icon";
 import {cirrusColors} from "../cirrusTheme";
 import Toolbar from "@material-ui/core/Toolbar/Toolbar";
-import classNames from 'classnames';
 import ComputeCredits from "./compute-credits/ComputeCredits";
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import GitHubPurchase from "./GitHubPurchase";
 
 
 const styles = theme => ({
@@ -29,99 +18,15 @@ const styles = theme => ({
   settingGap: {
     paddingTop: 16
   },
-  gap: {
-    paddingTop: 16
-  },
-  row: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  leftIcon: {
-    marginRight: theme.spacing.unit,
-  },
 });
-
-const ORGANIZATIONAL_PRIVATE_REPOSITORIES_PLAN_ID = 993;
 
 class GitHubOrganizationSettings extends React.Component {
   static contextTypes = {
     router: PropTypes.object
   };
 
-  state = {
-    showActiveUsers: false,
-  };
-
-
-  handleCloseActiveUsersDialog = () => {
-    this.setState({ showActiveUsers: false });
-  };
-
   render() {
-    let {info, organization, classes} = this.props;
-    let githubMarketplaceComponent = (
-      <div>
-        <Typography variant="subheading">
-          No GitHub Marketplace plan has been configured!
-        </Typography>
-      </div>
-    );
-    let actionButton = (
-      <Button variant="contained"
-              href={`https://github.com/marketplace/cirrus-ci/order/MDIyOk1hcmtldHBsYWNlTGlzdGluZ1BsYW45OTM=?account=${organization}`}>
-        <Icon className={classNames(classes.leftIcon, "fa", "fa-github")}/>
-        Purchase Plan for Private Repositories
-      </Button>
-    );
-    let cancelPlanButton = null;
-    let viewActiveUsers = null;
-    if (info.purchase && info.purchase.planId === ORGANIZATIONAL_PRIVATE_REPOSITORIES_PLAN_ID) {
-      githubMarketplaceComponent = (
-        <div>
-          <Typography variant="subheading">
-            Purchased GitHub Plan: <b>{info.purchase.planName}</b> for <b>{info.purchase.unitCount}</b> seats
-          </Typography>
-          <Typography variant="subheading">
-            Amount of monthly active users under the plan: <b>{info.activeUsersAmount}</b>
-          </Typography>
-          <Typography variant="subheading">
-            Available seats: <b>{info.purchase.unitCount - info.activeUsersAmount}</b>
-          </Typography>
-        </div>
-      );
-      viewActiveUsers = (
-        <Button variant="contained"
-                onClick={() => this.setState({ showActiveUsers: true })}>
-          <Icon className={classNames(classes.leftIcon)}>group</Icon>
-          View Active Users
-        </Button>
-      );
-      actionButton = (
-        <Button variant="contained"
-                href={`https://github.com/marketplace/cirrus-ci/order/MDIyOk1hcmtldHBsYWNlTGlzdGluZ1BsYW45OTM=?account=${organization}`}>
-          <Icon className={classNames(classes.leftIcon)}>group_add</Icon>
-          Add More Seats
-        </Button>
-      );
-      cancelPlanButton = (
-        <Button variant="contained"
-                href={`https://github.com/marketplace/cirrus-ci/order/MDIyOk1hcmtldHBsYWNlTGlzdGluZ1BsYW45OTA=?account=${organization}`}>
-          <Icon className={classNames(classes.leftIcon, "fa", "fa-github")}/>
-          Switch to Free Plan
-        </Button>
-      );
-    }
-
-    let trialComponent = null;
-    if (info.purchase && info.purchase.onFreeTrial && info.purchase.freeTrialDaysLeft > 0) {
-      trialComponent = (
-        <div className={classes.row}>
-          <Typography variant="subheading">
-            Days of Free Trial left: <b>{info.purchase.freeTrialDaysLeft}</b>
-          </Typography>
-        </div>
-      );
-    }
+    let {organization, classes} = this.props;
     return (
       <div>
         <Paper elevation={1}>
@@ -133,40 +38,13 @@ class GitHubOrganizationSettings extends React.Component {
         </Paper>
         <div className={classes.settingGap}/>
         <Paper elevation={1}>
-          <Card>
-            <CardHeader title="GitHub Settings"/>
-            <CardContent>
-              {githubMarketplaceComponent}
-              {trialComponent}
-            </CardContent>
-            <CardActions>
-              {cancelPlanButton}
-              {viewActiveUsers}
-              {actionButton}
-            </CardActions>
-          </Card>
+          <GitHubPurchase info={this.props.info}/>
         </Paper>
         <div className={classes.settingGap}/>
         <Paper elevation={1}>
           <ComputeCredits info={this.props.info}/>
         </Paper>
         <div className={classes.settingGap}/>
-        <Dialog
-          open={this.state.showActiveUsers}
-          onClose={this.handleCloseActiveUsersDialog}
-        >
-          <DialogTitle>Users active in the last 30 days</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              {info.activeUserNames.join("\n")}
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleCloseActiveUsersDialog} color="primary">
-              Close
-            </Button>
-          </DialogActions>
-        </Dialog>
       </div>
     );
   }
@@ -176,16 +54,7 @@ export default createFragmentContainer(withRouter(withStyles(styles)(GitHubOrgan
   info: graphql`
     fragment GitHubOrganizationSettings_info on GitHubOrganizationInfo {
       name
-      role
-      activeUsersAmount
-      activeUserNames
-      purchase {
-        planId
-        planName
-        unitCount
-        onFreeTrial
-        freeTrialDaysLeft
-      }
+      ...GitHubPurchase_info
       ...ComputeCredits_info
     }
   `,

b0b0a6f5423dbd7898bf81e94545686934bca821
Fedor Korotkov <fedor.korotkov@gmail.com>
2018-10-23 11:55:57 -0400

    update libraries

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './App';
 import './index.css';
 import './rtu/ConnectionManager'
 
-injectTapEventPlugin(); // for fancy material ui effects
 ReactDOM.render(<App/>, document.getElementById('root'));

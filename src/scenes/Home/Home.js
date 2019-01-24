937793f05b17eb34d61fede2bf76edaf0a241eec
Fedor Korotkov <fedor.korotkov@gmail.com>
2019-01-24 15:28:52 -0500

    Update src/scenes/Home/Home.js
    Co-Authored-By: RDIL <jumbocakefdx@gmail.com>

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -32,7 +32,7 @@ const Home = (props) => (
           <Paper elevation={1}>
             <Toolbar>
               <FormGroup>
-                <Typography variant="title">Please Log In to see your recent builds.  Newcomers please visit [our documentation](https://cirrus-ci.org).</Typography>
+                <ReactMarkdown variant="title">Please Log In to see your recent builds. Newcomers please visit [our documentation](https://cirrus-ci.org).</ReactMarkdown>
               </FormGroup>
             </Toolbar>
           </Paper>

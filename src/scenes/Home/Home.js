4888de278a387e1599b140164a8cbc88d0b327d6
Reece Dunham <me@rdil.rocks>
2019-03-16 12:13:12 -0400

    Bug fix

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -32,7 +32,7 @@ const Home = (props) => (
           <Paper elevation={1}>
             <Toolbar>
               <FormGroup>
-                <ReactMarkdown># Welcome to Cirrus CI.></ReactMarkdown>
+                <ReactMarkdown># Welcome to Cirrus CI.</ReactMarkdown>
                 <ReactMarkdown>Please [**sign in**](https://api.cirrus-ci.com/redirect/auth/github) to see your recent builds.</ReactMarkdown>
                 <ReactMarkdown>Just got here? Visit [**our documentation**](https://cirrus-ci.org).</ReactMarkdown>
               </FormGroup>

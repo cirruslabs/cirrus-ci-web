410bef16339f8bd80d98aa460c5ffe5f0c2aa109
Reece Dunham <me@rdil.rocks>
2019-03-18 10:25:51 -0400

    Create dangerfile.js

---
+++ dangerfile.js
@@ -0,0 +1,12 @@
+import {message, danger} from "danger"
+
+const modifiedMD = danger.git.modified_files.join("- ")
+message("Changed Files in this PR: \n - " + modifiedMD)
+
+const packageChanged = includes(danger.git.modified_files, 'package.json');
+const lockfileChanged = includes(danger.git.modified_files, 'yarn.lock');
+if (packageChanged && !lockfileChanged) {
+  const message = 'Changes were made to package.json, but not to yarn.lock';
+  const idea = 'Perhaps you need to run `yarn install`?';
+  warn(`${message} - <i>${idea}</i>`);
+}

30bab1d479ae689c84d473b1e422f7a3ff71629d
fedor <fedor.korotkov@gmai.com>
2019-10-10 20:56:30 -0700

    Support subscribing to multiple topics

--- src/createRelayEnvironment.ts
+++ src/createRelayEnvironment.ts
@@ -7,53 +7,44 @@ const { Environment, Network, RecordSource, Store, Observable } = require('relay
  */
 function subscription(operation, variables, cacheConfig) {
   if (variables['taskID'] && operation.text.indexOf('commands') > 0) {
-    let taskSubscriptionObserver = webSocketSubscription(
-      'TASK',
-      variables['taskID'],
-      operation,
-      variables,
-      cacheConfig,
-    );
-    let taskCommandsSubscriptionObserver = webSocketSubscription(
-      'TASK_COMMANDS',
-      variables['taskID'],
-      operation,
-      variables,
-      cacheConfig,
-    );
-    return taskSubscriptionObserver.concat(taskCommandsSubscriptionObserver);
+    return webSocketSubscriptions(operation, variables, [
+      ['TASK', variables['taskID']],
+      ['TASK_COMMANDS', variables['taskID']],
+    ]);
   } else if (variables['repositoryID'] && operation.text.indexOf('lastDefaultBranchBuild') > 0) {
-    return webSocketSubscription(
-      'REPOSITORY_DEFAULT_BRANCH_BUILD',
-      variables['repositoryID'],
-      operation,
-      variables,
-      cacheConfig,
-    );
+    return webSocketSubscriptions(operation, variables, [
+      ['REPOSITORY_DEFAULT_BRANCH_BUILD', variables['repositoryID']],
+    ]);
   } else if (variables['taskID']) {
-    return webSocketSubscription('TASK', variables['taskID'], operation, variables, cacheConfig);
+    return webSocketSubscriptions(operation, variables, [['TASK', variables['taskID']]]);
   } else if (variables['buildID']) {
-    return webSocketSubscription('BUILD', variables['buildID'], operation, variables, cacheConfig);
+    return webSocketSubscriptions(operation, variables, [['BUILD', variables['buildID']]]);
   }
 }
 
-function webSocketSubscription(kind, id, operation, variables, cacheConfig) {
+function webSocketSubscriptions(operation, variables, kind2id: Array<[string, string]>) {
   let dataSource = null;
 
-  let dispose = subscribeObjectUpdates(kind, id, () => {
-    fetchQuery(operation, variables).then(
-      response => {
-        dataSource && dataSource.next(response);
-      },
-      error => {
-        dataSource && dataSource.error(error);
-      },
-    );
+  let result = Observable.create(sink => {
+    dataSource = sink;
   });
 
-  return Observable.create(sink => {
-    dataSource = sink;
-  }).finally(dispose);
+  kind2id.forEach(kindIdPair => {
+    let [kind, id] = kindIdPair;
+    let dispose = subscribeObjectUpdates(kind, id, () => {
+      fetchQuery(operation, variables).then(
+        response => {
+          dataSource && dataSource.next(response);
+        },
+        error => {
+          dataSource && dataSource.error(error);
+        },
+      );
+    });
+    result = result.finally(dispose);
+  });
+
+  return result;
 }
 
 function fetchQuery(operation, variables) {

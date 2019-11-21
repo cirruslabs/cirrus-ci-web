9b2e0b87643c907263112f3ab84c7e59accdb24b
Reece Dunham <me@rdil.rocks>
2019-11-21 08:32:22 -0500

    Shorten

--- src/components/artifacts/ArtifactsView.tsx
+++ src/components/artifacts/ArtifactsView.tsx
@@ -194,7 +194,7 @@ class ArtifactsView extends React.Component<Props> {
       <Paper elevation={1}>
         <Toolbar className={classes.title}>
           <Typography variant="h6" color="inherit">
-            {this._currentPath() == null ? 'Artifacts' : this._currentPath()}
+            {this._currentPath() || 'Artifacts'}
           </Typography>
         </Toolbar>
         <List>{items}</List>

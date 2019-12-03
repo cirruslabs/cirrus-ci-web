ae205527f3f73a41b14bf97e4650a6d08215614b
Reece Dunham <me@rdil.rocks>
2019-12-03 16:24:14 +0000

    optimize

--- src/components/webhooks/DeliveryInfoDialog.tsx
+++ src/components/webhooks/DeliveryInfoDialog.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
 import DeliveryInfoDialogLazyContent from './DeliveryInfoDialogLazyContent';
@@ -14,22 +13,17 @@ interface Props {
   open: boolean;
 }
 
-class DeliveryInfoDialog extends React.Component<Props> {
-  render() {
-    const { delivery, ...other } = this.props;
-
-    return (
-      <Dialog {...other}>
-        <DialogTitle>Delivery Info for {delivery.id}</DialogTitle>
-        <DeliveryInfoDialogLazyContent deliveryId={delivery.id} />
-        <DialogActions>
-          <Button onClick={this.props.onClose} color="primary" variant="contained" autoFocus>
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }
-}
-
-export default withStyles({})(DeliveryInfoDialog);
+export default (props: Props) => {
+  const { delivery, ...other } = props;
+  return (
+    <Dialog {...other}>
+      <DialogTitle>Delivery Info for {delivery.id}</DialogTitle>
+      <DeliveryInfoDialogLazyContent deliveryId={delivery.id} />
+      <DialogActions>
+        <Button onClick={props.onClose} color="primary" variant="contained" autoFocus>
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};

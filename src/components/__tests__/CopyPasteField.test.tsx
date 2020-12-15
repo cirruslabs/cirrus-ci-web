7ab6b8a242dc42a49be0875620c2f2a9037eff2b
Fedor Korotkov <fedor.korotkov@gmail.com>
2020-12-15 14:16:15 -0500

    No need to test material UI

--- src/components/__tests__/CopyPasteField.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CopyPasteField from '../common/CopyPasteField';
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  const tree = renderer.create(<CopyPasteField name="my copy-paste field" value={'hello world'} />).toJSON();
-  expect(tree).toMatchSnapshot();
-});

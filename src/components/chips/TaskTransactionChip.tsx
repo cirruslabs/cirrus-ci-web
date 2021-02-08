d08fd2d4eeaff9ac36c45f48f717ab60546a9d85
fedor <fedor@cirruslabs.org>
2021-02-08 08:54:51 -0500

    TaskTransactionChip

--- src/components/chips/TaskTransactionChip.tsx
+++ src/components/chips/TaskTransactionChip.tsx
@@ -7,52 +7,49 @@ import Tooltip from '@material-ui/core/Tooltip';
 import { graphql } from 'babel-plugin-relay/macro';
 import { cirrusColorsState } from '../../cirrusTheme';
 import { isTaskFinalStatus } from '../../utils/status';
-import { withTheme, WithTheme } from '@material-ui/core/styles';
+import { withTheme } from '@material-ui/core/styles';
 import { createFragmentContainer } from 'react-relay';
 import { TaskTransactionChip_task } from './__generated__/TaskTransactionChip_task.graphql';
 import { useRecoilValue } from 'recoil';
 
-interface Props extends WithTheme {
+interface Props {
   task: TaskTransactionChip_task;
   className?: string;
 }
 
-class TaskTransactionChip extends React.Component<Props> {
-  render() {
-    const cirrusColors = useRecoilValue(cirrusColorsState);
+function TaskTransactionChip(props: Props) {
+  const cirrusColors = useRecoilValue(cirrusColorsState);
 
-    let { task } = this.props;
-
-    let { transaction, usedComputeCredits } = task;
-    if (!usedComputeCredits) {
-      return <div />;
-    }
-
-    let tip = isTaskFinalStatus(task.status)
-      ? 'No compute credits were charged!'
-      : 'Compute credit cost will be calculated at the end of the task.';
-    if (transaction) {
-      tip = `${transaction.creditsAmount} compute credits were charged for this task.`;
-    }
-
-    if (transaction && transaction.initialCreditsAmount) {
-      tip += ` (corrected from ${transaction.initialCreditsAmount})`;
-    }
-
-    return (
-      <Tooltip title={tip}>
-        <Chip
-          className={this.props.className}
-          label="compute credits"
-          avatar={
-            <Avatar style={{ backgroundColor: cirrusColors.success }}>
-              <Star style={{ color: this.props.theme.palette.background.paper }} />
-            </Avatar>
-          }
-        />
-      </Tooltip>
-    );
+  let task = props.task;
+  let { transaction, usedComputeCredits } = task;
+  if (!usedComputeCredits) {
+    return <div />;
   }
+
+  let tip = isTaskFinalStatus(task.status)
+    ? 'No compute credits were charged!'
+    : 'Compute credit cost will be calculated at the end of the task.';
+  if (transaction) {
+    tip = `${transaction.creditsAmount} compute credits were charged for this task.`;
+  }
+
+  if (transaction && transaction.initialCreditsAmount) {
+    tip += ` (corrected from ${transaction.initialCreditsAmount})`;
+  }
+
+  return (
+    <Tooltip title={tip}>
+      <Chip
+        className={this.props.className}
+        label="compute credits"
+        avatar={
+          <Avatar style={{ backgroundColor: cirrusColors.success }}>
+            <Star style={{ color: this.props.theme.palette.background.paper }} />
+          </Avatar>
+        }
+      />
+    </Tooltip>
+  );
 }
 
 export default createFragmentContainer(withTheme(TaskTransactionChip), {

0c9a53ca08320c7bd5bcbf81bb0991a46a1dc40c
fedor <fedor@cirruslabs.org>
2021-02-08 16:54:04 -0500

    TaskListRow to function

--- src/components/tasks/TaskListRow.tsx
+++ src/components/tasks/TaskListRow.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { useHistory, withRouter } from 'react-router-dom';
 
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
@@ -9,7 +9,6 @@ import TaskDurationChip from '../chips/TaskDurationChip';
 import { shorten } from '../../utils/text';
 import { createFragmentContainer } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
-import PropTypes from 'prop-types';
 import { navigateTask } from '../../utils/navigate';
 import { withStyles, WithStyles } from '@material-ui/core/styles';
 import classNames from 'classnames';
@@ -31,45 +30,36 @@ const styles = {
   },
 };
 
-interface Props extends WithStyles<typeof styles>, RouteComponentProps {
+interface Props extends WithStyles<typeof styles> {
   task: TaskListRow_task;
   showCreation: boolean;
 }
 
-class TaskListRow extends React.Component<Props> {
-  static contextTypes = {
-    router: PropTypes.object,
-  };
-
-  render() {
-    let { task, classes } = this.props;
-    return (
-      <TableRow
-        onClick={e => navigateTask(this.context.router.history, e, task.id)}
-        hover={true}
-        style={{ cursor: 'pointer' }}
-      >
-        <TableCell className={classNames(classes.cell)}>
-          <TaskNameChip task={task} className={classes.chip} />
-          {this.props.showCreation ? <TaskCreatedChip task={task} className={classes.chip} /> : null}
-          <TaskDurationChip task={task} className={classNames(classes.chip, 'd-none', 'd-md-none')} />
-        </TableCell>
-        <TableCell className={classNames('d-none', 'd-lg-table-cell', classes.cell)}>
-          {task.uniqueLabels.map(label => {
-            return <Chip key={label} className={classes.chip} label={shorten(label)} />;
-          })}
-        </TableCell>
-        <TableCell className={classes.cell}>
-          <div className="d-flex justify-content-end">
-            <TaskDurationChip task={task} className={classNames(classes.chip, classes.lastChip)} />
-          </div>
-        </TableCell>
-      </TableRow>
-    );
-  }
+function TaskListRow(props: Props) {
+  let history = useHistory();
+  let { task, classes } = props;
+  return (
+    <TableRow onClick={e => navigateTask(history, e, task.id)} hover={true} style={{ cursor: 'pointer' }}>
+      <TableCell className={classNames(classes.cell)}>
+        <TaskNameChip task={task} className={classes.chip} />
+        {props.showCreation ? <TaskCreatedChip task={task} className={classes.chip} /> : null}
+        <TaskDurationChip task={task} className={classNames(classes.chip, 'd-none', 'd-md-none')} />
+      </TableCell>
+      <TableCell className={classNames('d-none', 'd-lg-table-cell', classes.cell)}>
+        {task.uniqueLabels.map(label => {
+          return <Chip key={label} className={classes.chip} label={shorten(label)} />;
+        })}
+      </TableCell>
+      <TableCell className={classes.cell}>
+        <div className="d-flex justify-content-end">
+          <TaskDurationChip task={task} className={classNames(classes.chip, classes.lastChip)} />
+        </div>
+      </TableCell>
+    </TableRow>
+  );
 }
 
-export default createFragmentContainer(withStyles(styles)(withRouter(TaskListRow)), {
+export default createFragmentContainer(withStyles(styles)(TaskListRow), {
   task: graphql`
     fragment TaskListRow_task on Task {
       id

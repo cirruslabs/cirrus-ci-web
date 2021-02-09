18903db3feaf1c00c64d7662c11e5fc9e0a37ba5
fedor <fedor@cirruslabs.org>
2021-02-09 05:21:14 -0500

    BillingSettingsButton.tsx

--- src/components/compute-credits/BillingSettingsButton.tsx
+++ src/components/compute-credits/BillingSettingsButton.tsx
@@ -1,64 +1,44 @@
-import React from 'react';
-import { RouteComponentProps, withRouter } from 'react-router-dom';
+import React, { useState } from 'react';
 import { createFragmentContainer } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
-import PropTypes from 'prop-types';
 import AlarmOffIcon from '@material-ui/icons/AlarmOff';
 import AlarmOnIcon from '@material-ui/icons/AlarmOn';
 import Button from '@material-ui/core/Button';
 import BillingSettingsDialog from './BillingSettingsDialog';
 import { BillingSettingsButton_info } from './__generated__/BillingSettingsButton_info.graphql';
 
-interface State {
-  openDialog: boolean;
-}
-
-interface Props extends RouteComponentProps {
+interface Props {
   info: BillingSettingsButton_info;
   className?: string;
 }
 
-class BillingSettingsButton extends React.Component<Props, State> {
-  static contextTypes = {
-    router: PropTypes.object,
-  };
+function BillingSettingsButton(props: Props) {
+  let [openDialog, setOpenDialog] = useState(false);
+  let { info, className } = props;
+  if (!info) return null;
 
-  state = { openDialog: false };
+  let { billingSettings } = info;
+  if (!billingSettings) return null;
 
-  toggleDialog = () => {
-    this.setState(prevState => ({
-      ...prevState,
-      openDialog: !prevState.openDialog,
-    }));
-  };
-
-  render() {
-    let { info, className } = this.props;
-    if (!info) return null;
-
-    let { billingSettings } = info;
-    if (!billingSettings) return null;
-
-    return (
-      <div className={className}>
-        <Button
-          variant="contained"
-          onClick={this.toggleDialog}
-          startIcon={billingSettings.enabled ? <AlarmOnIcon /> : <AlarmOffIcon />}
-        >
-          {billingSettings.enabled ? 'Edit Auto Pay' : 'Set up Auto Pay'}
-        </Button>
-        <BillingSettingsDialog
-          billingSettings={billingSettings}
-          open={this.state.openDialog}
-          onClose={this.toggleDialog}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className={className}>
+      <Button
+        variant="contained"
+        onClick={() => setOpenDialog(!openDialog)}
+        startIcon={billingSettings.enabled ? <AlarmOnIcon /> : <AlarmOffIcon />}
+      >
+        {billingSettings.enabled ? 'Edit Auto Pay' : 'Set up Auto Pay'}
+      </Button>
+      <BillingSettingsDialog
+        billingSettings={billingSettings}
+        open={openDialog}
+        onClose={() => setOpenDialog(!openDialog)}
+      />
+    </div>
+  );
 }
 
-export default createFragmentContainer(withRouter(BillingSettingsButton), {
+export default createFragmentContainer(BillingSettingsButton, {
   info: graphql`
     fragment BillingSettingsButton_info on GitHubOrganizationInfo {
       billingSettings {

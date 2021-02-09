21feb1b49d22091bb3d07565c6b86cc2b382baac
fedor <fedor@cirruslabs.org>
2021-02-09 05:11:42 -0500

    LastDefaultBranchBuildRow.tsx

--- src/components/builds/LastDefaultBranchBuildRow.tsx
+++ src/components/builds/LastDefaultBranchBuildRow.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import { createFragmentContainer, Disposable, requestSubscription } from 'react-relay';
+import React, { useEffect } from 'react';
+import { createFragmentContainer, requestSubscription } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
 import environment from '../../createRelayEnvironment';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import { withStyles, WithStyles } from '@material-ui/core/styles';
-import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { useHistory, withRouter } from 'react-router-dom';
 import { navigateRepository } from '../../utils/navigate';
 import RepositoryNameChip from '../chips/RepositoryNameChip';
 import BuildStatusChip from '../chips/BuildStatusChip';
 import classNames from 'classnames';
 import BuildChangeChip from '../chips/BuildChangeChip';
-import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import { LastDefaultBranchBuildRow_repository } from './__generated__/LastDefaultBranchBuildRow_repository.graphql';
 
@@ -36,74 +35,62 @@ const styles = theme => ({
   },
 });
 
-interface Props extends WithStyles<typeof styles>, RouteComponentProps {
+interface Props extends WithStyles<typeof styles> {
   repository: LastDefaultBranchBuildRow_repository;
 }
 
-class LastDefaultBranchBuildRow extends React.Component<Props> {
-  static contextTypes = {
-    router: PropTypes.object,
-  };
+function LastDefaultBranchBuildRow(props: Props) {
+  useEffect(() => {
+    let variables = { repositoryID: props.repository.id };
 
-  subscription: Disposable;
-
-  componentDidMount() {
-    let variables = { repositoryID: this.props.repository.id };
-
-    this.subscription = requestSubscription(environment, {
+    let subscription = requestSubscription(environment, {
       subscription: buildSubscription,
       variables: variables,
     });
-  }
+    return () => {
+      subscription.dispose();
+    };
+  }, []);
 
-  componentWillUnmount() {
-    this.closeSubscription();
+  let history = useHistory();
+  let { classes, repository } = props;
+  let build = repository.lastDefaultBranchBuild;
+  if (!build) {
+    return null;
   }
-
-  closeSubscription() {
-    this.subscription && this.subscription.dispose && this.subscription.dispose();
-  }
-
-  render() {
-    let { classes, repository } = this.props;
-    let build = repository.lastDefaultBranchBuild;
-    if (!build) {
-      return null;
-    }
-    return (
-      <TableRow
-        key={repository.id}
-        onClick={e => navigateRepository(this.context.router.history, e, repository.owner, repository.name)}
-        hover={true}
-        style={{ cursor: 'pointer' }}
-      >
-        <TableCell className={classes.cell}>
-          <div className="d-flex flex-column align-items-start">
-            <RepositoryNameChip repository={repository} className={classes.chip} />
-            <BuildChangeChip build={repository.lastDefaultBranchBuild} className={classes.chip} />
-          </div>
-          <div className={classNames('d-lg-none', classes.message)}>
-            <Typography variant="body1" color="inherit">
-              {build.changeMessageTitle}
-            </Typography>
-          </div>
-        </TableCell>
-        <TableCell className={classNames(classes.cell, classes.message)}>
-          <div className="card-body">
-            <Typography variant="body1" color="inherit">
-              {build.changeMessageTitle}
-            </Typography>
-          </div>
-        </TableCell>
-        <TableCell className={classes.cell}>
-          <BuildStatusChip build={build} className={classNames('pull-right', classes.chip)} />
-        </TableCell>
-      </TableRow>
-    );
-  }
+  return (
+    <TableRow
+      key={repository.id}
+      onClick={e => navigateRepository(history, e, repository.owner, repository.name)}
+      hover={true}
+      style={{ cursor: 'pointer' }}
+    >
+      <TableCell className={classes.cell}>
+        <div className="d-flex flex-column align-items-start">
+          <RepositoryNameChip repository={repository} className={classes.chip} />
+          <BuildChangeChip build={repository.lastDefaultBranchBuild} className={classes.chip} />
+        </div>
+        <div className={classNames('d-lg-none', classes.message)}>
+          <Typography variant="body1" color="inherit">
+            {build.changeMessageTitle}
+          </Typography>
+        </div>
+      </TableCell>
+      <TableCell className={classNames(classes.cell, classes.message)}>
+        <div className="card-body">
+          <Typography variant="body1" color="inherit">
+            {build.changeMessageTitle}
+          </Typography>
+        </div>
+      </TableCell>
+      <TableCell className={classes.cell}>
+        <BuildStatusChip build={build} className={classNames('pull-right', classes.chip)} />
+      </TableCell>
+    </TableRow>
+  );
 }
 
-export default createFragmentContainer(withStyles(styles)(withRouter(LastDefaultBranchBuildRow)), {
+export default createFragmentContainer(withStyles(styles)(LastDefaultBranchBuildRow), {
   repository: graphql`
     fragment LastDefaultBranchBuildRow_repository on Repository {
       id

fa67ba9d2927ca9686c90b1ca909ed744b71f10d
Nikolay Edigaryev <edigaryev@gmail.com>
2021-08-10 17:51:37 +0300

    Make .cirrus.star evaluation errors more obvious (#389)
    * Make .cirrus.star evaluation errors more obvious
    
    * There can be issues that are not errors

--- src/components/builds/ConfigurationWithIssues.tsx
+++ src/components/builds/ConfigurationWithIssues.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { createStyles, Theme } from '@material-ui/core';
 import { WithStyles, withStyles } from '@material-ui/core/styles';
 import { Alert } from '@material-ui/lab';
@@ -39,6 +39,12 @@ const styles = (theme: Theme) =>
       fontSize: theme.typography.fontSize,
       borderRadius: '0',
     },
+    columnFlexDirection: {
+      flexDirection: 'column',
+    },
+    topPadded: {
+      paddingTop: theme.spacing(2),
+    },
   });
 
 interface Props extends WithStyles<typeof styles> {
@@ -52,35 +58,37 @@ function ConfigurationWithIssues(props: Props) {
     return null;
   }
 
-  // Sort issues by (line, column) and store them in a map for faster access
-  var sortedIssues = build.parsingResult.issues
-    .filter(issue => {
-      return issue.path.length === 0 || issue.path.endsWith('.cirrus.yml') || issue.path.endsWith('.cirrus.yaml');
-    })
-    .sort(function (left, right) {
-      return left.line - right.line || left.column - right.column;
-    });
+  // cacheIssues sorts issues by (line, column) and stores them in a map for faster access.
+  function cacheIssues(issues: Array<any>): Map<number, any> {
+    let issueMap = new Map();
 
-  let issueMap = {};
+    issues
+      .sort(function (left, right) {
+        return left.line - right.line || left.column - right.column;
+      })
+      .forEach(issue => {
+        let targetList = issueMap.get(issue.line);
 
-  for (let issue of sortedIssues) {
-    let targetList = issueMap[issue.line] || (issueMap[issue.line] = []);
-    targetList.push(issue);
+        if (targetList === undefined) {
+          issueMap.set(issue.line, [issue]);
+        } else {
+          targetList.push(issue);
+        }
+      });
+
+    return issueMap;
   }
 
-  // Render configuration with issues
-  function getIssuesForLine(lineNumber: number) {
-    const issuesForLine = issueMap[lineNumber];
+  function getIssuesForLine(issueCache: Map<number, any>, lineNumber: number) {
+    const issuesForLine = issueCache.get(lineNumber);
 
     if (issuesForLine === undefined || issuesForLine.length === 0) return null;
 
-    const renderedIssues = issuesForLine.map(issue => {
-      return (
-        <Alert className={classes.issue} severity={issue.level.toLowerCase()}>
-          {issue.message}
-        </Alert>
-      );
-    });
+    const renderedIssues = issuesForLine.map(issue => (
+      <Alert className={classes.issue} severity={issue.level.toLowerCase()}>
+        {issue.message}
+      </Alert>
+    ));
 
     return (
       <tr>
@@ -89,28 +97,98 @@ function ConfigurationWithIssues(props: Props) {
     );
   }
 
-  const lines = build.parsingResult.processedYamlConfig.split('\n').map((lineContent, zeroBasedLineNumber) => [
-    <tr>
-      <td className={classes.lineNumber}>{zeroBasedLineNumber + 1}</td>
-      <td className={classes.lineContent}>
-        <span>{lineContent}</span>
-      </td>
-    </tr>,
-    getIssuesForLine(zeroBasedLineNumber + 1),
-  ]);
+  function generateTable(configuration, issueCache: Map<number, any>): ReactNode {
+    if (issueCache.size === 0) return null;
 
-  const errorIssue = build.parsingResult.issues.find(it => it.level === 'ERROR');
-  const summaryText = errorIssue ? 'Failed to parse configuration' : 'Issues found while parsing configuration';
+    const tableContents = configuration.split('\n').map((lineContent, zeroBasedLineNumber) => [
+      <tr>
+        <td className={classes.lineNumber}>{zeroBasedLineNumber + 1}</td>
+        <td className={classes.lineContent}>
+          <span>{lineContent}</span>
+        </td>
+      </tr>,
+      getIssuesForLine(issueCache, zeroBasedLineNumber + 1),
+    ]);
+
+    const extraLines = issueCache.get(0)
+      ? issueCache.get(0).map(issue => [
+          <Alert className={classes.issue} severity={issue.level.toLowerCase()}>
+            {issue.message}
+          </Alert>,
+        ])
+      : null;
+
+    return (
+      <>
+        <table className={classes.configurationTable} cellPadding={0}>
+          <tbody>{tableContents}</tbody>
+        </table>
+        {extraLines != null && <br />}
+        {extraLines}
+      </>
+    );
+  }
+
+  const yamlIssues = build.parsingResult.issues.filter(
+    issue => issue.path.endsWith('.cirrus.yml') || issue.path.endsWith('.cirrus.yaml'),
+  );
+  const yamlIssueCache = cacheIssues(yamlIssues);
+  const yamlTable = generateTable(build.parsingResult.processedYamlConfig, yamlIssueCache);
+
+  const starlarkIssues = build.parsingResult.issues.filter(issue => issue.path.endsWith('.cirrus.star'));
+  const starlarkIssueCache = cacheIssues(starlarkIssues);
+  const starlarkTable = generateTable(build.parsingResult.rawStarlarkConfig, starlarkIssueCache);
+
+  let yamlTitle = (
+    <Typography variant="subtitle1">
+      <p>YAML configuration</p>
+    </Typography>
+  );
+  let starlarkTitle = (
+    <Typography variant="subtitle1" className={classes.topPadded}>
+      <p>Starlark configuration</p>
+    </Typography>
+  );
+
+  let summaryText: string;
+
+  const yamlHasErrors = yamlIssues.some(it => it.level === 'ERROR');
+  const yamlHasIssues = yamlIssues.some(it => it.level !== 'ERROR');
+  const starlarkHasErrors = starlarkIssues.some(it => it.level === 'ERROR');
+  const starlarkHasIssues = starlarkIssues.some(it => it.level !== 'ERROR');
+
+  if (yamlHasErrors && starlarkHasErrors) {
+    summaryText = 'Failed to parse YAML and Starlark configurations';
+  } else if (yamlHasIssues && starlarkHasIssues) {
+    summaryText = 'Found issues while parsing YAML and Starlark configurations';
+  } else if (yamlHasErrors) {
+    summaryText = 'Failed to parse YAML configuration';
+    yamlTitle = null;
+    starlarkTitle = null;
+  } else if (starlarkHasErrors) {
+    summaryText = 'Failed to evaluate Starlark configuration';
+    yamlTitle = null;
+    starlarkTitle = null;
+  } else if (yamlHasIssues) {
+    summaryText = 'Found issues while parsing YAML configuration';
+    yamlTitle = null;
+    starlarkTitle = null;
+  } else if (starlarkHasIssues) {
+    summaryText = 'Found issues while evaluating Starlark configuration';
+    yamlTitle = null;
+    starlarkTitle = null;
+  }
 
   return (
-    <Accordion defaultExpanded={errorIssue != null}>
+    <Accordion defaultExpanded={yamlHasErrors || starlarkHasErrors}>
       <AccordionSummary expandIcon={<ExpandMoreIcon />}>
         <Typography variant="h6">{summaryText}</Typography>
       </AccordionSummary>
-      <AccordionDetails>
-        <table className={classes.configurationTable} cellPadding={0}>
-          <tbody>{lines}</tbody>
-        </table>
+      <AccordionDetails className={classes.columnFlexDirection}>
+        {yamlTitle}
+        {yamlTable}
+        {starlarkTitle}
+        {starlarkTable}
       </AccordionDetails>
     </Accordion>
   );
@@ -120,6 +198,7 @@ export default createFragmentContainer(withStyles(styles)(ConfigurationWithIssue
   build: graphql`
     fragment ConfigurationWithIssues_build on Build {
       parsingResult {
+        rawStarlarkConfig
         processedYamlConfig
         issues {
           level

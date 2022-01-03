5b3981892e8d30ac28a218dd55330b99d7b98eee
fedor <fedor.korotkov@gmail.com>
2022-01-03 17:44:04 -0500

    Workaround https://github.com/cirruslabs/cirrus-ci-web/issues/421

--- src/components/common/MarkdownTypography.tsx
+++ src/components/common/MarkdownTypography.tsx
@@ -1,4 +1,4 @@
-import { Typography, TypographyProps } from '@mui/material';
+import { Link, Typography, TypographyProps } from '@mui/material';
 import ReactMarkdown from 'react-markdown';
 
 interface MarkdownTypographyProps extends TypographyProps {
@@ -12,6 +12,15 @@ const MarkdownTypography = (props: MarkdownTypographyProps) => {
     <ReactMarkdown
       children={text}
       components={{
+        a: ({ ...props }) => {
+          // eslint-disable-next-line
+          if (props.href === 'javascript:void(0)') {
+            // workaround https://github.com/cirruslabs/cirrus-ci-web/issues/421
+            // when <> with any :: inside treated as a link
+            return <>{'<' + props.children[0] + '>'}</>;
+          }
+          return <Link {...props} />;
+        },
         p: el => <Typography {...otherProps} children={el.children} />,
       }}
     />

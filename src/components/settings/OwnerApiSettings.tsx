b76ee33c6214e05c4caaf6df7dfbbd941cb677d8
fedor <fedor.korotkov@gmail.com>
2022-08-09 09:49:36 -0400

    Fixed showing of API token
    Related to #452

--- src/components/settings/OwnerApiSettings.tsx
+++ src/components/settings/OwnerApiSettings.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import { createFragmentContainer } from 'react-relay';
+import React, { useState } from 'react';
+import { commitMutation, createFragmentContainer } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
-import ApiSettingsBase from './ApiSettingsBase';
 import { OwnerApiSettings_info } from './__generated__/OwnerApiSettings_info.graphql';
-import { GenerateNewOwnerAccessTokenInput } from './__generated__/OwnerApiSettingsMutation.graphql';
+import {
+  GenerateNewOwnerAccessTokenInput,
+  OwnerApiSettingsMutationResponse,
+} from './__generated__/OwnerApiSettingsMutation.graphql';
+import createStyles from '@mui/styles/createStyles';
+import environment from '../../createRelayEnvironment';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import { Link } from '@mui/material';
+import { withStyles, WithStyles } from '@mui/styles';
 
 const generateNewTokenMutation = graphql`
   mutation OwnerApiSettingsMutation($input: GenerateNewOwnerAccessTokenInput!) {
@@ -13,30 +26,90 @@ const generateNewTokenMutation = graphql`
   }
 `;
 
-interface Props {
+const styles = theme =>
+  createStyles({
+    textField: {
+      width: '100%',
+      marginLeft: theme.spacing(1.0),
+      marginRight: theme.spacing(1.0),
+    },
+  });
+
+interface Props extends WithStyles<typeof styles> {
   info: OwnerApiSettings_info;
 }
 
 function OwnerApiSettings(props: Props) {
-  function getMutationVariables() {
+  let { classes } = props;
+  let existingTokenComponent = null;
+  let [newToken, setNewToken] = useState(null);
+
+  function generateNewAccessToken() {
     let input: GenerateNewOwnerAccessTokenInput = {
       clientMutationId: `generate-api-token-${props.info.uid}`,
       platform: props.info.platform,
       ownerUid: props.info.uid,
     };
-    return { input };
+
+    commitMutation(environment, {
+      mutation: generateNewTokenMutation,
+      variables: { input },
+      onCompleted: (response: OwnerApiSettingsMutationResponse) => {
+        setNewToken(response.generateNewOwnerAccessToken.token);
+      },
+      onError: err => console.error(err),
+    });
   }
 
+  if (props.info.apiToken.maskedToken) {
+    existingTokenComponent = (
+      <Typography variant="subtitle1">Currently active token: {props.info.apiToken.maskedToken}</Typography>
+    );
+  }
+  let newTokenComponent = null;
+  if (newToken) {
+    newTokenComponent = (
+      <TextField
+        label="New Access Token"
+        value={`Make sure to copy your new access token now. You won't be able to see it again!\n\n${newToken}`}
+        className={classes.textField}
+        margin="normal"
+        variant="outlined"
+        multiline
+      />
+    );
+  }
+
+  let cardActions = (
+    <CardActions>
+      <Button variant="contained" onClick={() => generateNewAccessToken()}>
+        Generate New Token
+      </Button>
+    </CardActions>
+  );
+
   return (
-    <ApiSettingsBase
-      generateNewTokenMutation={generateNewTokenMutation}
-      getMutationVariables={getMutationVariables}
-      maskedToken={props.info.apiToken?.maskedToken}
-    />
+    <div>
+      <Card elevation={24}>
+        <CardHeader title="API Settings" />
+        <CardContent>
+          <Typography variant="subtitle1">
+            Need an API token for scripts or testing? Generate an access token for the Cirrus CI API here. See the{' '}
+            <Link color="inherit" href="https://cirrus-ci.org/api/">
+              documentation
+            </Link>{' '}
+            for more details.
+          </Typography>
+          {existingTokenComponent}
+          {newTokenComponent}
+        </CardContent>
+        {cardActions}
+      </Card>
+    </div>
   );
 }
 
-export default createFragmentContainer(OwnerApiSettings, {
+export default createFragmentContainer(withStyles(styles)(OwnerApiSettings), {
   info: graphql`
     fragment OwnerApiSettings_info on OwnerInfo {
       platform

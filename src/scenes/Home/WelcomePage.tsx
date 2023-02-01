bde7ac9e37732be13d518dc0f660b9f28cfc3b50
Fedor Korotkov <fedor.korotkov@gmail.com>
2023-02-01 09:56:19 -0500

    Fixed Welcome Styles (#511)

--- src/scenes/Home/WelcomePage.tsx
+++ src/scenes/Home/WelcomePage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Paper from '@mui/material/Paper';
-import { Helmet as Head } from 'react-helmet';
+import {Helmet as Head} from 'react-helmet';
 import MarkdownTypography from '../../components/common/MarkdownTypography';
-import { makeStyles } from '@mui/styles';
-makeStyles(theme => {
+import {makeStyles} from '@mui/styles';
+
+const useStyles = makeStyles(theme => {
   return {
     paper: {
       padding: theme.spacing(3.0),
@@ -11,17 +12,21 @@ makeStyles(theme => {
     },
   };
 });
-let welcomePage = props => (
-  <Paper className={props.classes.paper}>
-    <Head>
-      <title>Welcome - Cirrus CI</title>
-    </Head>
-    <MarkdownTypography text={'# Welcome to Cirrus CI.'} />
-    <MarkdownTypography
-      text={'Please [**sign in**](https://api.cirrus-ci.com/redirect/auth/github) to see your recent builds.'}
-    />
-    <MarkdownTypography text={'Just got here? Visit [**our documentation**](https://cirrus-ci.org).'} />
-  </Paper>
-);
 
-export default welcomePage;
+function WelcomePage() {
+  let classes = useStyles();
+  return (
+    <Paper className={classes.paper}>
+      <Head>
+        <title>Welcome - Cirrus CI</title>
+      </Head>
+      <MarkdownTypography text={'# Welcome to Cirrus CI.'}/>
+      <MarkdownTypography
+        text={'Please [**sign in**](https://api.cirrus-ci.com/redirect/auth/github) to see your recent builds.'}
+      />
+      <MarkdownTypography text={'Just got here? Visit [**our documentation**](https://cirrus-ci.org).'}/>
+    </Paper>
+  );
+}
+
+export default WelcomePage;

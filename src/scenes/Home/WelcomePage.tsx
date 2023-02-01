09e8c80daed00e5be862f10afb0bba490971fff4
fedor <fedor.korotkov@gmail.com>
2023-02-01 10:01:49 -0500

    Prettier

--- src/scenes/Home/WelcomePage.tsx
+++ src/scenes/Home/WelcomePage.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Paper from '@mui/material/Paper';
-import {Helmet as Head} from 'react-helmet';
+import { Helmet as Head } from 'react-helmet';
 import MarkdownTypography from '../../components/common/MarkdownTypography';
-import {makeStyles} from '@mui/styles';
+import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles(theme => {
   return {
@@ -20,11 +20,11 @@ function WelcomePage() {
       <Head>
         <title>Welcome - Cirrus CI</title>
       </Head>
-      <MarkdownTypography text={'# Welcome to Cirrus CI.'}/>
+      <MarkdownTypography text={'# Welcome to Cirrus CI.'} />
       <MarkdownTypography
         text={'Please [**sign in**](https://api.cirrus-ci.com/redirect/auth/github) to see your recent builds.'}
       />
-      <MarkdownTypography text={'Just got here? Visit [**our documentation**](https://cirrus-ci.org).'}/>
+      <MarkdownTypography text={'Just got here? Visit [**our documentation**](https://cirrus-ci.org).'} />
     </Paper>
   );
 }

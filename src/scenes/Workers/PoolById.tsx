099d1ed7320a0af9bc2540b36de193320170f953
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2023-02-03 17:16:48 +0100

    PoolById

--- src/scenes/Workers/PoolById.tsx
+++ src/scenes/Workers/PoolById.tsx
@@ -1,37 +1,30 @@
 import React from 'react';
-
-import { QueryRenderer } from 'react-relay';
+import { useLazyLoadQuery } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
+import { useParams } from 'react-router-dom';
 
-import environment from '../../createRelayEnvironment';
-import CirrusLinearProgress from '../../components/common/CirrusLinearProgress';
 import NotFound from '../NotFound';
 import PoolDetails from '../../components/workers/PoolDetails';
+
 import { PoolByIdQuery } from './__generated__/PoolByIdQuery.graphql';
-import { useParams } from 'react-router-dom';
 
 export default function PoolById(): JSX.Element {
   let { poolId } = useParams();
-  return (
-    <QueryRenderer<PoolByIdQuery>
-      environment={environment}
-      variables={{ poolId }}
-      query={graphql`
-        query PoolByIdQuery($poolId: ID!) {
-          persistentWorkerPool(poolId: $poolId) {
-            ...PoolDetails_pool
-          }
+
+  const response = useLazyLoadQuery<PoolByIdQuery>(
+    graphql`
+      query PoolByIdQuery($poolId: ID!) {
+        persistentWorkerPool(poolId: $poolId) {
+          ...PoolDetails_pool
         }
-      `}
-      render={({ error, props }) => {
-        if (!props) {
-          return <CirrusLinearProgress />;
-        }
-        if (!props.persistentWorkerPool) {
-          return <NotFound message={error} />;
-        }
-        return <PoolDetails pool={props.persistentWorkerPool} />;
-      }}
-    />
+      }
+    `,
+    { poolId },
   );
+
+  // todo: pass error message to <NotFound>
+  if (!response.persistentWorkerPool) {
+    return <NotFound />;
+  }
+  return <PoolDetails pool={response.persistentWorkerPool} />;
 }

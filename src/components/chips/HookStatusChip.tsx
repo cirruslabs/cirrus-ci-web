46381a618c3ca9d38fe9f606a813f238e9fb4b9e
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2023-02-03 13:44:32 +0100

    HookStatusChip

--- src/components/chips/HookStatusChip.tsx
+++ src/components/chips/HookStatusChip.tsx
@@ -5,18 +5,30 @@ import Chip from '@mui/material/Chip';
 import Icon from '@mui/material/Icon';
 import { useHookStatusColor } from '../../utils/colors';
 import { hookIconName, hookStatusMessage } from '../../utils/status';
-import { createFragmentContainer } from 'react-relay';
+import { useFragment } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
 import { useTheme } from '@mui/material';
-import { HookStatusChip_hook } from './__generated__/HookStatusChip_hook.graphql';
+import { HookStatusChip_hook$key } from './__generated__/HookStatusChip_hook.graphql';
 
 interface Props {
-  hook: HookStatusChip_hook;
+  hook: HookStatusChip_hook$key;
   className?: string;
 }
 
-function HookStatusChip(props: Props) {
-  let { hook, className } = props;
+export default function HookStatusChip(props: Props) {
+  let hook = useFragment(
+    graphql`
+      fragment HookStatusChip_hook on Hook {
+        info {
+          error
+          durationNanos
+        }
+      }
+    `,
+    props.hook,
+  );
+
+  let { className } = props;
   let theme = useTheme();
   return (
     <Chip
@@ -30,14 +42,3 @@ function HookStatusChip(props: Props) {
     />
   );
 }
-
-export default createFragmentContainer(HookStatusChip, {
-  hook: graphql`
-    fragment HookStatusChip_hook on Hook {
-      info {
-        error
-        durationNanos
-      }
-    }
-  `,
-});

18724ab8c0730001e86620581146335065cfbeee
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2023-02-22 12:52:07 +0100

    TaskStatusChipExtended + WorkerStatusChip update

--- src/components/chips/TaskStatusChipExtended.tsx
+++ src/components/chips/TaskStatusChipExtended.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
 
+import { useFragment } from 'react-relay';
 import Avatar from '@mui/material/Avatar';
 import Chip from '@mui/material/Chip';
 import Icon from '@mui/material/Icon';
 import Tooltip from '@mui/material/Tooltip';
 import { useTaskStatusColor } from '../../utils/colors';
 import { taskStatusIconName } from '../../utils/status';
-import { createFragmentContainer } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
-import { TaskStatusChipExtended_task } from './__generated__/TaskStatusChipExtended_task.graphql';
+import { TaskStatusChipExtended_task$key } from './__generated__/TaskStatusChipExtended_task.graphql';
 import { navigateTaskHelper } from '../../utils/navigateHelper';
 import { useNavigate } from 'react-router-dom';
 import { useTheme } from '@mui/material';
 
 interface Props {
-  task: TaskStatusChipExtended_task;
+  task: TaskStatusChipExtended_task$key;
   className?: string;
   key?: string;
 }
 
-function TaskStatusChipExtended(props: Props) {
-  let { task, className, key } = props;
+export default function TaskStatusChipExtended(props: Props) {
+  let task = useFragment(
+    graphql`
+      fragment TaskStatusChipExtended_task on Task {
+        id
+        name
+        status
+        durationInSeconds
+        executingTimestamp
+        repository {
+          owner
+          name
+        }
+      }
+    `,
+    props.task,
+  );
+  let { className, key } = props;
   let navigate = useNavigate();
   let theme = useTheme();
   let hasExecutingTimestamp = task.executingTimestamp && task.executingTimestamp > 0;
@@ -48,19 +64,3 @@ function TaskStatusChipExtended(props: Props) {
   }
   return chip;
 }
-
-export default createFragmentContainer(TaskStatusChipExtended, {
-  task: graphql`
-    fragment TaskStatusChipExtended_task on Task {
-      id
-      name
-      status
-      durationInSeconds
-      executingTimestamp
-      repository {
-        owner
-        name
-      }
-    }
-  `,
-});

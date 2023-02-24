e4892e11bc47d80ab239a16158373762fc07ba22
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2023-02-24 12:45:40 +0100

    Add useLazyLoadQuery to Task

--- src/scenes/Task/Task.tsx
+++ src/scenes/Task/Task.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import { QueryRenderer } from 'react-relay';
+import { useLazyLoadQuery } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
 
-import environment from '../../createRelayEnvironment';
 import TaskDetails from '../../components/tasks/TaskDetails';
-import CirrusLinearProgress from '../../components/common/CirrusLinearProgress';
 import NotFound from '../NotFound';
 import { TaskQuery } from './__generated__/TaskQuery.graphql';
 import { useParams } from 'react-router-dom';
@@ -13,35 +11,29 @@ import AppBreadcrumbs from '../../components/common/AppBreadcrumbs';
 
 export default function Task(): JSX.Element {
   let { taskId } = useParams();
+
+  const response = useLazyLoadQuery<TaskQuery>(
+    graphql`
+      query TaskQuery($taskId: ID!) {
+        task(id: $taskId) {
+          ...TaskDetails_task
+          ...AppBreadcrumbs_task
+        }
+        viewer {
+          ...AppBreadcrumbs_viewer
+        }
+      }
+    `,
+    { taskId },
+  );
+
+  if (!response.task) {
+    return <NotFound />;
+  }
   return (
-    <QueryRenderer<TaskQuery>
-      environment={environment}
-      variables={{ taskId }}
-      query={graphql`
-        query TaskQuery($taskId: ID!) {
-          task(id: $taskId) {
-            ...TaskDetails_task
-            ...AppBreadcrumbs_task
-          }
-          viewer {
-            ...AppBreadcrumbs_viewer
-          }
-        }
-      `}
-      render={({ error, props }) => {
-        if (!props) {
-          return <CirrusLinearProgress />;
-        }
-        if (!props.task) {
-          return <NotFound message={error} />;
-        }
-        return (
-          <>
-            <AppBreadcrumbs task={props.task} viewer={props.viewer} />
-            <TaskDetails task={props.task} />
-          </>
-        );
-      }}
-    />
+    <>
+      <AppBreadcrumbs task={response.task} viewer={response.viewer} />
+      <TaskDetails task={response.task} />
+    </>
   );
 }

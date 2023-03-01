d784946561048f56fb5e7ec637a0643c8867f721
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2023-03-01 18:21:52 +0100

    Add useFragment to TaskRerunnerChip

--- src/components/chips/TaskRerunnerChip.tsx
+++ src/components/chips/TaskRerunnerChip.tsx
@@ -3,29 +3,30 @@ import React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Chip from '@mui/material/Chip';
 
-import { createFragmentContainer } from 'react-relay';
+import { useFragment } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
-import { TaskRerunnerChip_task } from './__generated__/TaskRerunnerChip_task.graphql';
+import { TaskRerunnerChip_task$key } from './__generated__/TaskRerunnerChip_task.graphql';
 
 interface Props {
-  task: TaskRerunnerChip_task;
+  task: TaskRerunnerChip_task$key;
   className?: string;
 }
 
-function TaskRerunnerChip(props: Props) {
-  let { reranBy } = props.task;
+export default function TaskRerunnerChip(props: Props) {
+  let task = useFragment(
+    graphql`
+      fragment TaskRerunnerChip_task on Task {
+        reranBy {
+          avatarURL
+        }
+      }
+    `,
+    props.task,
+  );
+
+  let { reranBy } = task;
 
   if (!reranBy) return <></>;
 
   return <Chip className={props.className} label="Re-ran by a user" avatar={<Avatar src={reranBy.avatarURL} />} />;
 }
-
-export default createFragmentContainer(TaskRerunnerChip, {
-  task: graphql`
-    fragment TaskRerunnerChip_task on Task {
-      reranBy {
-        avatarURL
-      }
-    }
-  `,
-});

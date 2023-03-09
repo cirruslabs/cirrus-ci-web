c958fecc098c53acc80409b548165cc014ed7e1a
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2023-03-09 13:28:30 +0100

    Add useLazyLoadQuery to Repository

--- src/scenes/Repository/Repository.tsx
+++ src/scenes/Repository/Repository.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import { QueryRenderer } from 'react-relay';
+import { useLazyLoadQuery } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
 
-import environment from '../../createRelayEnvironment';
 import RepositoryBuildList from '../../components/repositories/RepositoryBuildList';
-import CirrusLinearProgress from '../../components/common/CirrusLinearProgress';
 import NotFound from '../NotFound';
 import { RepositoryQuery } from './__generated__/RepositoryQuery.graphql';
 import { useParams } from 'react-router-dom';
@@ -16,39 +14,33 @@ export default function Repository(): JSX.Element {
   let params = useParams();
   let { repositoryId } = params;
   let branch = params['*'];
+
+  const response = useLazyLoadQuery<RepositoryQuery>(
+    graphql`
+      query RepositoryQuery($repositoryId: ID!, $branch: String) {
+        repository(id: $repositoryId) {
+          ...AppBreadcrumbs_repository
+          ...RepositoryBuildList_repository @arguments(branch: $branch)
+        }
+      }
+    `,
+    { repositoryId, branch },
+  );
+
+  if (!response.repository) {
+    let notFoundMessage = (
+      <MarkdownTypography
+        text={
+          'Repository not found! Please [install Cirrus CI](https://cirrus-ci.org/guide/quick-start/) or push a [`.cirrus.yml`](https://cirrus-ci.org/guide/writing-tasks/)!'
+        }
+      />
+    );
+    return <NotFound messageComponent={notFoundMessage} />;
+  }
   return (
-    <QueryRenderer<RepositoryQuery>
-      environment={environment}
-      variables={{ repositoryId, branch }}
-      query={graphql`
-        query RepositoryQuery($repositoryId: ID!, $branch: String) {
-          repository(id: $repositoryId) {
-            ...AppBreadcrumbs_repository
-            ...RepositoryBuildList_repository @arguments(branch: $branch)
-          }
-        }
-      `}
-      render={({ error, props }) => {
-        if (!props) {
-          return <CirrusLinearProgress />;
-        }
-        if (!props.repository) {
-          let notFoundMessage = (
-            <MarkdownTypography
-              text={
-                'Repository not found! Please [install Cirrus CI](https://cirrus-ci.org/guide/quick-start/) or push a [`.cirrus.yml`](https://cirrus-ci.org/guide/writing-tasks/)!'
-              }
-            />
-          );
-          return <NotFound messageComponent={notFoundMessage} />;
-        }
-        return (
-          <>
-            <AppBreadcrumbs repository={props.repository} />
-            <RepositoryBuildList repository={props.repository} />
-          </>
-        );
-      }}
-    />
+    <>
+      <AppBreadcrumbs repository={response.repository} />
+      <RepositoryBuildList repository={response.repository} />
+    </>
   );
 }

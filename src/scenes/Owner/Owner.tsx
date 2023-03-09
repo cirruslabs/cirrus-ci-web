6c863010de6fc28522c13bac7eb39fbcb83eea63
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2023-03-09 19:35:47 +0100

    Add useLazyLoadQuery to Owner

--- src/scenes/Owner/Owner.tsx
+++ src/scenes/Owner/Owner.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 
-import { QueryRenderer } from 'react-relay';
+import { useLazyLoadQuery } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
 
-import environment from '../../createRelayEnvironment';
-import CirrusLinearProgress from '../../components/common/CirrusLinearProgress';
 import { OwnerQuery } from './__generated__/OwnerQuery.graphql';
 import { useParams } from 'react-router-dom';
 import OwnerRepositoryList from '../../components/account/OwnerRepositoryList';
@@ -13,35 +11,29 @@ import AppBreadcrumbs from '../../components/common/AppBreadcrumbs';
 
 export default function Owner(): JSX.Element {
   let { platform, owner } = useParams();
+
+  const response = useLazyLoadQuery<OwnerQuery>(
+    graphql`
+      query OwnerQuery($platform: String!, $owner: String!) {
+        ownerInfoByName(platform: $platform, name: $owner) {
+          ...OwnerRepositoryList_info
+          ...AppBreadcrumbs_info
+        }
+        viewer {
+          ...AppBreadcrumbs_viewer
+        }
+      }
+    `,
+    { platform, owner },
+  );
+
+  if (!response.ownerInfoByName) {
+    return <NotFound />;
+  }
   return (
-    <QueryRenderer<OwnerQuery>
-      environment={environment}
-      variables={{ platform, owner }}
-      query={graphql`
-        query OwnerQuery($platform: String!, $owner: String!) {
-          ownerInfoByName(platform: $platform, name: $owner) {
-            ...OwnerRepositoryList_info
-            ...AppBreadcrumbs_info
-          }
-          viewer {
-            ...AppBreadcrumbs_viewer
-          }
-        }
-      `}
-      render={({ error, props }) => {
-        if (!props) {
-          return <CirrusLinearProgress />;
-        }
-        if (!props.ownerInfoByName) {
-          return <NotFound />;
-        }
-        return (
-          <>
-            <AppBreadcrumbs info={props.ownerInfoByName} viewer={props.viewer} />
-            <OwnerRepositoryList info={props.ownerInfoByName} />
-          </>
-        );
-      }}
-    />
+    <>
+      <AppBreadcrumbs info={response.ownerInfoByName} viewer={response.viewer} />
+      <OwnerRepositoryList info={response.ownerInfoByName} />
+    </>
   );
 }

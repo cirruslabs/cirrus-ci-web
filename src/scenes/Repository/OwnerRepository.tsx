9c2bfa62f0e04e5ebae7078ff13b0133b2c717c1
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2023-03-09 19:51:05 +0100

    Add useLazyLoadQuery to OwnerRepository

--- src/scenes/Repository/OwnerRepository.tsx
+++ src/scenes/Repository/OwnerRepository.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import { QueryRenderer } from 'react-relay';
+import { useLazyLoadQuery } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
 
-import environment from '../../createRelayEnvironment';
 import RepositoryBuildList from '../../components/repositories/RepositoryBuildList';
-import CirrusLinearProgress from '../../components/common/CirrusLinearProgress';
 import NotFound from '../NotFound';
 import { OwnerRepositoryQuery } from './__generated__/OwnerRepositoryQuery.graphql';
 import { useParams } from 'react-router-dom';
@@ -16,42 +14,36 @@ export default function OwnerRepository(): JSX.Element {
   let params = useParams();
   let { platform, owner, name } = params;
   let branch = params['*'];
+
+  const response = useLazyLoadQuery<OwnerRepositoryQuery>(
+    graphql`
+      query OwnerRepositoryQuery($platform: String!, $owner: String!, $name: String!, $branch: String) {
+        ownerRepository(platform: $platform, owner: $owner, name: $name) {
+          ...AppBreadcrumbs_repository
+          ...RepositoryBuildList_repository @arguments(branch: $branch)
+        }
+        viewer {
+          ...AppBreadcrumbs_viewer
+        }
+      }
+    `,
+    { platform, owner, name, branch },
+  );
+
+  if (!response.ownerRepository) {
+    let notFoundMessage = (
+      <MarkdownTypography
+        text={
+          'Repository not found! Please [install Cirrus CI](https://cirrus-ci.org/guide/quick-start/) or push a [`.cirrus.yml`](https://cirrus-ci.org/guide/writing-tasks/)!'
+        }
+      />
+    );
+    return <NotFound messageComponent={notFoundMessage} />;
+  }
   return (
-    <QueryRenderer<OwnerRepositoryQuery>
-      environment={environment}
-      variables={{ platform, owner, name, branch }}
-      query={graphql`
-        query OwnerRepositoryQuery($platform: String!, $owner: String!, $name: String!, $branch: String) {
-          ownerRepository(platform: $platform, owner: $owner, name: $name) {
-            ...AppBreadcrumbs_repository
-            ...RepositoryBuildList_repository @arguments(branch: $branch)
-          }
-          viewer {
-            ...AppBreadcrumbs_viewer
-          }
-        }
-      `}
-      render={({ error, props }) => {
-        if (!props) {
-          return <CirrusLinearProgress />;
-        }
-        if (!props.ownerRepository) {
-          let notFoundMessage = (
-            <MarkdownTypography
-              text={
-                'Repository not found! Please [install Cirrus CI](https://cirrus-ci.org/guide/quick-start/) or push a [`.cirrus.yml`](https://cirrus-ci.org/guide/writing-tasks/)!'
-              }
-            />
-          );
-          return <NotFound messageComponent={notFoundMessage} />;
-        }
-        return (
-          <>
-            <AppBreadcrumbs repository={props.ownerRepository} viewer={props.viewer} branch={branch} />
-            <RepositoryBuildList repository={props.ownerRepository} branch={branch} />
-          </>
-        );
-      }}
-    />
+    <>
+      <AppBreadcrumbs repository={response.ownerRepository} viewer={response.viewer} branch={branch} />
+      <RepositoryBuildList repository={response.ownerRepository} branch={branch} />
+    </>
   );
 }

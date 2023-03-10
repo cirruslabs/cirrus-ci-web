4531474f0e3042173f90e3be36e0a2878cea3217
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2023-03-10 13:47:26 +0100

    Add useLazyLoadQuery to RepositorySettings

--- src/scenes/RepositorySettings/RepositorySettings.tsx
+++ src/scenes/RepositorySettings/RepositorySettings.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 
-import { QueryRenderer } from 'react-relay';
+import { useLazyLoadQuery } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
 
-import environment from '../../createRelayEnvironment';
-import CirrusLinearProgress from '../../components/common/CirrusLinearProgress';
 import RepositorySettingsPage from '../../components/repositories/RepositorySettingsPage';
 import NotFound from '../NotFound';
 import { RepositorySettingsQuery } from './__generated__/RepositorySettingsQuery.graphql';
@@ -14,44 +12,38 @@ import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 
 export default function RepositorySettings(): JSX.Element {
   let { repositoryId } = useParams();
+
+  const response = useLazyLoadQuery<RepositorySettingsQuery>(
+    graphql`
+      query RepositorySettingsQuery($repositoryId: ID!) {
+        repository(id: $repositoryId) {
+          ...AppBreadcrumbs_repository
+          ...RepositorySettingsPage_repository
+        }
+        viewer {
+          ...AppBreadcrumbs_viewer
+        }
+      }
+    `,
+    { repositoryId },
+  );
+
+  if (!response.repository) {
+    return <NotFound />;
+  }
   return (
-    <QueryRenderer<RepositorySettingsQuery>
-      environment={environment}
-      variables={{ repositoryId }}
-      query={graphql`
-        query RepositorySettingsQuery($repositoryId: ID!) {
-          repository(id: $repositoryId) {
-            ...AppBreadcrumbs_repository
-            ...RepositorySettingsPage_repository
-          }
-          viewer {
-            ...AppBreadcrumbs_viewer
-          }
-        }
-      `}
-      render={({ error, props }) => {
-        if (!props) {
-          return <CirrusLinearProgress />;
-        }
-        if (!props.repository) {
-          return <NotFound message={error} />;
-        }
-        return (
-          <>
-            <AppBreadcrumbs
-              repository={props.repository}
-              viewer={props.viewer}
-              extraCrumbs={[
-                {
-                  name: 'Repository Settings',
-                  Icon: SettingsOutlinedIcon,
-                },
-              ]}
-            />
-            <RepositorySettingsPage repository={props.repository} />
-          </>
-        );
-      }}
-    />
+    <>
+      <AppBreadcrumbs
+        repository={response.repository}
+        viewer={response.viewer}
+        extraCrumbs={[
+          {
+            name: 'Repository Settings',
+            Icon: SettingsOutlinedIcon,
+          },
+        ]}
+      />
+      <RepositorySettingsPage repository={response.repository} />
+    </>
   );
 }

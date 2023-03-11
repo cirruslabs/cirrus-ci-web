879b879bf19e35b635e9cc6399de591169d5ac85
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2023-03-11 23:18:09 +0100

    Add useFragment to OwnerComputeCredits

--- src/components/compute-credits/OwnerComputeCredits.tsx
+++ src/components/compute-credits/OwnerComputeCredits.tsx
@@ -1,40 +1,23 @@
 import { graphql } from 'babel-plugin-relay/macro';
 import React from 'react';
-import { createPaginationContainer, RelayPaginationProp } from 'react-relay';
+import { useFragment } from 'react-relay';
 import ComputeCreditsBase from './ComputeCreditsBase';
-import { OwnerComputeCredits_info } from './__generated__/OwnerComputeCredits_info.graphql';
+import { OwnerComputeCredits_info$key } from './__generated__/OwnerComputeCredits_info.graphql';
 import ComputeCreditsTransactionsList from './ComputeCreditsTransactionsList';
 
 interface Props {
-  relay: RelayPaginationProp;
-  info: OwnerComputeCredits_info;
+  info: OwnerComputeCredits_info$key;
 }
 
-let OwnerComputeCredits = (props: Props) => {
-  return (
-    <ComputeCreditsBase
-      ownerUid={props.info.uid}
-      balanceInCredits={props.info.balanceInCredits}
-      info={props.info}
-      transactionsComponent={
-        <ComputeCreditsTransactionsList transactions={props.info.transactions.edges.map(edge => edge.node)} />
-      }
-    />
-  );
-};
-
-export default createPaginationContainer(
-  OwnerComputeCredits,
-  {
-    info: graphql`
-      fragment OwnerComputeCredits_info on OwnerInfo
-      @argumentDefinitions(count: { type: "Int", defaultValue: 50 }, cursor: { type: "String" })
-      @refetchable(queryName: "") {
+export default function OwnerComputeCredits(props: Props) {
+  let info = useFragment(
+    graphql`
+      fragment OwnerComputeCredits_info on OwnerInfo {
         uid
         name
         balanceInCredits
         ...ComputeCreditsBase_info
-        transactions(last: $count, after: $cursor) @connection(key: "OwnerComputeCredits_transactions") {
+        transactions(last: 50) {
           edges {
             node {
               taskId
@@ -44,33 +27,18 @@ export default createPaginationContainer(
         }
       }
     `,
-  },
-  {
-    direction: 'forward',
-    getConnectionFromProps(props) {
-      return props.info && props.info.transactions;
-    },
-    // This is also the default implementation of `getFragmentVariables` if it isn't provided.
-    getFragmentVariables(prevVars, totalCount) {
-      return {
-        ...prevVars,
-        count: totalCount,
-      };
-    },
-    getVariables(props: any, { count, cursor }, fragmentVariables) {
-      return {
-        count: count,
-        cursor: cursor,
-        platform: props.info.platform,
-        uid: props.info.uid,
-      };
-    },
-    query: graphql`
-      query OwnerComputeCreditsQuery($platform: String!, $uid: ID!, $count: Int!, $cursor: String) {
-        ownerInfo(platform: $platform, uid: $uid) {
-          ...OwnerComputeCredits_info @arguments(count: $count, cursor: $cursor)
-        }
+
+    props.info,
+  );
+
+  return (
+    <ComputeCreditsBase
+      ownerUid={info.uid}
+      balanceInCredits={info.balanceInCredits}
+      info={info}
+      transactionsComponent={
+        <ComputeCreditsTransactionsList transactions={info.transactions.edges.map(edge => edge.node)} />
       }
-    `,
-  },
-);
+    />
+  );
+}

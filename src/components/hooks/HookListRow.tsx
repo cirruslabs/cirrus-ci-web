9de527d0438d03000e59752dc36fb04e440c79cc
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2023-03-13 19:18:25 +0100

    Add useFragment to HookListRow

--- src/components/hooks/HookListRow.tsx
+++ src/components/hooks/HookListRow.tsx
@@ -3,12 +3,12 @@ import { useNavigate } from 'react-router-dom';
 
 import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
-import { createFragmentContainer } from 'react-relay';
+import { useFragment } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
 import { navigateHookHelper } from '../../utils/navigateHelper';
 import { makeStyles } from '@mui/styles';
 import classNames from 'classnames';
-import { HookListRow_hook } from './__generated__/HookListRow_hook.graphql';
+import { HookListRow_hook$key } from './__generated__/HookListRow_hook.graphql';
 import HookStatusChip from '../chips/HookStatusChip';
 import HookNameChip from '../chips/HookNameChip';
 import HookCreatedChip from '../chips/HookCreatedChip';
@@ -31,11 +31,22 @@ const useStyles = makeStyles(theme => {
 });
 
 interface Props {
-  hook: HookListRow_hook;
+  hook: HookListRow_hook$key;
 }
 
-function HookListRow(props: Props) {
-  let { hook } = props;
+export default function HookListRow(props: Props) {
+  let hook = useFragment(
+    graphql`
+      fragment HookListRow_hook on Hook {
+        id
+        ...HookStatusChip_hook
+        ...HookCreatedChip_hook
+        ...HookNameChip_hook
+      }
+    `,
+    props.hook,
+  );
+
   let classes = useStyles();
   let navigate = useNavigate();
 
@@ -49,14 +60,3 @@ function HookListRow(props: Props) {
     </TableRow>
   );
 }
-
-export default createFragmentContainer(HookListRow, {
-  hook: graphql`
-    fragment HookListRow_hook on Hook {
-      id
-      ...HookStatusChip_hook
-      ...HookCreatedChip_hook
-      ...HookNameChip_hook
-    }
-  `,
-});

9ca6988a4410bbe78cebfe59ff61ffcd1cf7626c
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2023-03-13 17:32:25 +0100

    Add useLazyLoadQuery to ViewerProfile

--- src/scenes/Profile/ViewerProfile.tsx
+++ src/scenes/Profile/ViewerProfile.tsx
@@ -1,35 +1,26 @@
 import React from 'react';
 
-import { QueryRenderer } from 'react-relay';
+import { useLazyLoadQuery } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
 
-import environment from '../../createRelayEnvironment';
-import CirrusLinearProgress from '../../components/common/CirrusLinearProgress';
 import Typography from '@mui/material/Typography';
 import UserProfile from '../../components/account/UserProfile';
 import { ViewerProfileQuery } from './__generated__/ViewerProfileQuery.graphql';
 
 export default function ViewerProfile(): JSX.Element {
-  return (
-    <QueryRenderer<ViewerProfileQuery>
-      environment={environment}
-      query={graphql`
-        query ViewerProfileQuery {
-          viewer {
-            ...UserProfile_user
-          }
+  const response = useLazyLoadQuery<ViewerProfileQuery>(
+    graphql`
+      query ViewerProfileQuery {
+        viewer {
+          ...UserProfile_user
         }
-      `}
-      variables={{}}
-      render={({ props }) => {
-        if (!props) {
-          return <CirrusLinearProgress />;
-        }
-        if (!props.viewer) {
-          return <Typography variant="subtitle1">Please sign in to see your profile!</Typography>;
-        }
-        return <UserProfile user={props.viewer} />;
-      }}
-    />
+      }
+    `,
+    {},
   );
+
+  if (!response.viewer) {
+    return <Typography variant="subtitle1">Please sign in to see your profile!</Typography>;
+  }
+  return <UserProfile user={response.viewer} />;
 }

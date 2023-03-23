068c3352352c38ad98f72374d65b5355d46a97f7
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2023-03-23 19:53:45 +0100

    New component BuildBranchNameChipNew

---
+++ src/components/chips/BuildBranchNameChipNew.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { useFragment } from 'react-relay';
+import { useNavigate } from 'react-router-dom';
+import { graphql } from 'babel-plugin-relay/macro';
+
+import { makeStyles } from '@mui/styles';
+import { Tooltip } from '@mui/material';
+import { deepOrange } from '@mui/material/colors';
+import Chip from '@mui/material/Chip';
+import CallSplitIcon from '@mui/icons-material/CallSplit';
+import UnarchiveIcon from '@mui/icons-material/UnarchiveOutlined';
+
+import { shorten } from '../../utils/text';
+import { navigateHelper } from '../../utils/navigateHelper';
+
+import { BuildBranchNameChipNew_build$key } from './__generated__/BuildBranchNameChipNew_build.graphql';
+
+const useStyles = makeStyles(theme => {
+  return {
+    chip: {
+      '& .MuiChip-avatar': {
+        height: 18,
+        width: 18,
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(-1),
+        color: deepOrange[500],
+      },
+    },
+  };
+});
+
+interface Props {
+  build: BuildBranchNameChipNew_build$key;
+}
+
+export default function BuildBranchNameChipNew(props: Props) {
+  let build = useFragment(
+    graphql`
+      fragment BuildBranchNameChipNew_build on Build {
+        id
+        branch
+        tag
+        repository {
+          id
+          owner
+          name
+        }
+      }
+    `,
+    props.build,
+  );
+
+  let classes = useStyles();
+  let navigate = useNavigate();
+
+  function handleBranchClick(event) {
+    if (build.repository) {
+      navigateHelper(
+        navigate,
+        event,
+        '/github/' + build.repository.owner + '/' + build.repository.name + '/' + build.branch,
+      );
+    } else if (build.repository.id) {
+      navigateHelper(navigate, event, '/repository/' + build.repository.id + '/' + build.branch);
+    }
+  }
+
+  if (build.tag) {
+    return (
+      <Tooltip title={`${build.tag} tag`}>
+        <Chip
+          className={classes.chip}
+          label={shorten(build.branch)}
+          avatar={<UnarchiveIcon />}
+          size="small"
+          onClick={handleBranchClick}
+          onAuxClick={handleBranchClick}
+        />
+      </Tooltip>
+    );
+  }
+
+  return (
+    <Chip
+      className={classes.chip}
+      label={shorten(build.branch)}
+      avatar={<CallSplitIcon />}
+      size="small"
+      onClick={handleBranchClick}
+      onAuxClick={handleBranchClick}
+    />
+  );
+}

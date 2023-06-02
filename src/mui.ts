4e580d216aae23abc6468bd23603e0e4edce18ac
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2023-06-02 16:17:22 +0200

    Sort values by alphabetic order

--- src/mui.ts
+++ src/mui.ts
@@ -1,38 +1,38 @@
-import { makeStyles } from '@mui/styles';
 import { useTheme } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 
-import Stack from '@mui/material/Stack';
-import Card from '@mui/material/Card';
-import Divider from '@mui/material/Divider';
-import CardActionArea from '@mui/material/CardActionArea';
-import Link from '@mui/material/Link';
-import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 
-import Settings from '@mui/icons-material/Settings';
 import GitHub from '@mui/icons-material/GitHub';
+import Settings from '@mui/icons-material/Settings';
 
 const mui = {
-  useTheme,
   makeStyles,
+  useTheme,
 
-  Stack,
-  Card,
-  Divider,
-  CardActionArea,
-  Link,
-  Box,
   Avatar,
+  Box,
+  Card,
+  CardActionArea,
+  Divider,
   IconButton,
+  Link,
+  Stack,
   Tooltip,
   Typography,
 
   icons: {
-    Settings,
     GitHub,
+    Settings,
   },
 };
 

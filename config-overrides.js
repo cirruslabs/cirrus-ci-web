0da39c83c5036c4c39584cccad8d0b71dcff078a
Nikolay Edigaryev <edigaryev@gmail.com>
2024-06-05 16:47:43 +0400

    yarn.lock: update dependencies (#604)
    * yarn.lock: update dependencies
    
    * Include 'vm' polyfill

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,7 @@ module.exports = function override(config) {
     crypto: require.resolve('crypto-browserify'),
     buffer: require.resolve('buffer/'),
     stream: require.resolve('stream-browserify'),
+    vm: require.resolve('vm-browserify'),
   };
 
   return config;
